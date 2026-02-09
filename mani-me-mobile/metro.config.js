733c102941797c42af40e302d6645df42a965725
ManiMe Developer <developer@manime.com>
2026-02-09 22:52:38 +0000

    fix: add punycode polyfill for React Native (whatwg-url compatibility)

--- mani-me-mobile/metro.config.js
+++ mani-me-mobile/metro.config.js
@@ -1,4 +1,12 @@
 // metro.config.js
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
+
+// Polyfill Node.js built-in modules not available in React Native
+config.resolver.extraNodeModules = {
+  ...config.resolver.extraNodeModules,
+  punycode: require.resolve('punycode'),
+};
+
+module.exports = config;
